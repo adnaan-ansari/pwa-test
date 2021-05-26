23d7c945dc4800ec4c4c996e16942744fff64dcc
adnaan-ansari <78425049+adnaan-ansari@users.noreply.github.com>
2021-05-26 11:13:40 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
-        '/index.htm',
-        '/style69.css'
+        '/pwa-test/index.htm',
+        '/pwa-test/style69.css'
       ]).then(() => self.skipWaiting());
     })
   );
